7336190a7eaab0a288b4971f8256fdf50451ded4
relubwu <bwu@ucsd.edu>
2019-04-03 11:34:30 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * NPM Packages
+ * @see https://gulpjs.com
+ */
+const fs = require('fs');
+const path = require('path');
+const { src, dest, parallel } = require('gulp');
+const less = require('gulp-less');
+
+export async function html() {
+
+}
+
+export async function css() {
+
+}
+
+export async function js() {
+  
+}
