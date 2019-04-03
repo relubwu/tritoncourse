8381d135e12269b2ed65d331d5ef2a04e5fb0aef
relubwu <bwu@ucsd.edu>
2019-04-03 12:29:05 -0700

    SASS removed due to closed source customized version .scss of pingendo

--- gulpfile.js
+++ gulpfile.js
@@ -11,28 +11,9 @@ sass.compiler = require('node-sass');
 
 const html = async () => {
   return src('template/*.pug')
-      .pipe(pug({
-        data: {
-          favicon: "",
-          title: "",
-          description: "",
-          keywords: "",
-          navLinks: [{ url: "", name: "" }],
-          courseInfo: "",
-          staff: [{ avatar: "", name: "", role: "" }],
-          location: "",
-          geoLocation: ""
-        }
-      }))
-      .pipe(dest('build/html'))
-}
-
-const css = async () => {
-  return src('template/wireframe.scss')
-    .pipe(sass())
-    .pipe(dest('build/css'))
+      .pipe(pug({ data: require('./config.json') }))
+      .pipe(dest('build'))
 }
 
 exports.html = html;
-exports.css = css;
-exports.default = parallel(html, css);
+exports.default = parallel(html);
